dff56bd8fd797a6ac77edd27e55e0c60cf2a1db0
SebRob3 <js.roblesm1@uniandes.edu.co>
2025-07-04 21:44:27 -0500

    end

--- src/logica/Automovil.java
+++ src/logica/Automovil.java
@@ -8,7 +8,6 @@ public class Automovil extends Vehiculo {
 
 	@Override
 	public double calculateAutonomia() {
-		// TODO Auto-generated method stub
 		return (15*Math.pow(10, 4))/((this.cilindraje + this.peso)) * (this.tamanioTanque);
 	}
 
